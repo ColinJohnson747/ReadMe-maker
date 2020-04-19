8eec6b4ae3e65f4ce23625587ff266b68627ccad
Colin Johnson <blisk76@gmail.com>
2020-04-18 23:16:48 -0400

    repo finder

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,27 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-];
+function usernameAndAvatar() {
+  return inquirer
+    .prompt({
+      message: "Enter your GitHub username:",
+      name: "username",
+    })
+    .then(function ({ username }) {
+      const queryUrl = `https://api.github.com/users/${username}/repos`;
 
-function writeToFile(fileName, data) {
+      axios.get(queryUrl).then(function (res) {
+        let amountOfRepos = res.data.length;
+
+        let chosenRepoPosition = Math.floor(
+          Math.random() * (amountOfRepos + 1)
+        );
+
+        const exampleRepo = res.data[chosenRepoPosition].name;
+        //const avatar = res.data[chosenRepoPosition].avatar_url
+      });
+    });
 }
 
-function init() {
-
-}
-
-init();
+usernameAndAvatar();
--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,7 +1,21 @@
-const api = {
-  getUser(username) {
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-  }
-};
+function getUser(username) {
+  inquirer
+    .prompt({
+      message: "Enter your GitHub username:",
+      name: "username",
+    })
+    .then(function ({ username }) {
+      const queryUrl = `https://api.github.com/users/${username}/events/public`;
 
-module.exports = api;
+      axios.get(queryUrl).then(function (res) {
+        console.log(res.data);
+      });
+    });
+}
+
+
+
+module.exports = getUser();
